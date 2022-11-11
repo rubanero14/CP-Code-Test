210fc0d880b8d30004b5cf80b3dda418cb0a7619
Rajaruban Rajindram <ahyat888@gmail.com>
2022-11-11 20:33:19 +0800

    Add project files.

---
+++ CP-Code-Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CP_Code_Test.MvcApplication" Language="C#" %>
---
+++ CP-DB/dbo/Tables/ConsultantInfo.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [ConsultantInfo](
+	[ConsultantInfoId] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](max) NULL,
+	[LastName] [nvarchar](max) NULL,
+	[EmailAddress] [nvarchar](255) NULL,
+	[MobileNo] [nvarchar](50) NULL,
+ CONSTRAINT [PK_ConsultantInfo] PRIMARY KEY CLUSTERED 
+(
+	[ConsultantInfoId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
